ebd03a5149189971f3232cc75628df928e22e378
FZSGBall <692732163@qq.com>
2023-07-25 14:32:34 +0800

    Update: 适配新加入的参数方法

--- SimpleLauncher/Commands/SLCommands.cs
+++ SimpleLauncher/Commands/SLCommands.cs
@@ -12,8 +12,9 @@ public class SLCommands
         "  |子命令1: -java | -j | -Java: 列出本机所有的Java环境\n" +
         "  |用法: list -java | list -j | list -Java\n" +
         "  |\n" +
-        "  |子命令2: -core | -c | -Core: 列出启动器路径下的\".minecraft\"文件夹中的所有核心\n" +
-        "  |用法: list -core | list -c | list -Core\n" +
+        "  |子命令2: -core[=<filter>] | -c | -Core: 列出启动器路径下的\".minecraft\"文件夹中的所有核心 (筛选器语法在'-c' 与 '-Core'子命令中也相同)\n" +
+        "  |用法: 需求举例: 只列举出所有release版本\n" +
+        "  |  | list -core=release\n" +
         "  |\n" +
         "  |注意事项: 该命令不可缺省子命令, 否则会报出错误\n" +
         "  |  | 举例: 'list' 用法会报错误: '对于该命令输入了过少的参数...'; 而 'list -java' 不会\n" +
@@ -30,7 +31,7 @@ public class SLCommands
                 new string[3] { "-core", "-c", "-Core" },
                 "",
                 null,
-                new SLCore.Command.Action(null, ActionType.Full)
+                new SLCore.Command.Action(ListSubAction.SUB_CMD_CORE, ActionType.Full)
             )
         },
         new SLCore.Command.Action(null, ActionType.Seg)

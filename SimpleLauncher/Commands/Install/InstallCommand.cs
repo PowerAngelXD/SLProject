f91d490c94ebc7ed8655279cc41c45fbaef2bd44
FZSGBall <692732163@qq.com>
2023-07-28 16:40:43 +0800

    Update: 更改了Install的帮助文档

--- SimpleLauncher/Commands/Install/InstallCommand.cs
+++ SimpleLauncher/Commands/Install/InstallCommand.cs
@@ -18,22 +18,20 @@ internal sealed class InstallCommand : ISLCommand
 
     public string HelpContent =>
         $"install | i | Install: 下载指定的资源 (目前仅限于: 原版核心，Forge，Fabric，Quilt，Java，Optifine){Environment.NewLine}" +
-        $"  |子命令1: java[ --filter <type>+<version>+<platform>(+kind)] | j | Java: 下载指定的java (筛选器语法在'j' 与 'Java'子命令中也相同){Environment.NewLine}" +
-        $"  |用法: 需求举例: 下载jdk17, 目标平台为linux: {Environment.NewLine}" +
-        $"  |  | install java --filter jdk+17+linux{Environment.NewLine}" +
+        $"  |子命令1: java[ --<version>] | j | Java: 下载指定的java (筛选器语法在'j' 与 'Java'子命令中也相同){Environment.NewLine}" +
+        $"  |用法: 需求举例: 下载jdk17: {Environment.NewLine}" +
+        $"  |  | install java --jdk17{Environment.NewLine}" +
         $"  |  | 注意事项: 如果所给筛选器指向不正确或者语法错误，SimpleLauncher会对您错误的操作报出错误{Environment.NewLine}" +
         $"  |  -{Environment.NewLine}" +
         $"  |子命令1 -- 附录: 筛选器语法: {Environment.NewLine}" +
-        $"  | <type> 可选项: jdk, java{Environment.NewLine}" +
-        $"  | <version> 可选项: 所有已知的版本均可，注意不带小数点，均为正整数{Environment.NewLine}" +
-        $"  | <platform> 可选项: win, linux, mac{Environment.NewLine}" +
-        $"  | (kind) 可选项: installer, pkg{Environment.NewLine}" +
+        $"  | <version> 可选项: jdk8, jdk11, jdk17, jdk18{Environment.NewLine}" +
         $"  |{Environment.NewLine}" +
         $"  |子命令2: core[ --filter <coreID>+<modifier>*] | c | Core: 下载根据筛选器修饰的版本，并按照筛选器对其进行配置 (筛选器语法在'c' 与 'Core'子命令中也相同){Environment.NewLine}" +
         $"  |用法: 需求举例1: 下载一个1.18.2的核心并使用Fabric安装器: {Environment.NewLine}" +
         $"  |  | install core --filter 1.18.2+fabric{Environment.NewLine}" +
         $"  | 需求举例2: 下载一个1.7.10的核心并使用Forge安装器，同时搭配Optifine: {Environment.NewLine}" +
-        $"  |  | install core --filter 1.7.10+forge+optifine";
+        $"  |  | install core --filter 1.7.10+forge+optifine{Environment.NewLine}" +
+        $"  -";
 
     public Task<ISLCommand?> ExecuteAsync(IEnumerable<string> args)
     {

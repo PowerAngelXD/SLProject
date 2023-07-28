cf3ad85640a65968f12c7a017bcf97a169e154d4
FZSGBall <692732163@qq.com>
2023-07-28 22:45:05 +0800

    Update: 完成了Install命令下Core的子命令的filter的reader

---
+++ SimpleLauncher/Commands/Install/Util/CoreFilterReader.cs
@@ -0,0 +1,82 @@
+namespace SimpleLauncher.Commands.Install.Util;
+
+
+public enum TokenType
+{
+    VersionId,   // 版本说明符
+    Identifier,  // 标识符，比如fabric，forge这种
+    ConnectOp    // 连接符，特指 ‘+’
+}
+
+/// <summary>
+/// ICFE: Install-Core Filter Expression的缩写
+/// </summary>
+public class ICFEToken
+{
+    public string? Content { get; set; }
+    public TokenType Type { get; set; }
+
+    public ICFEToken(string content, TokenType type)
+    {
+        Content = content;
+        Type = type;
+    }
+}
+
+public class CoreFilterReader
+{
+    private List<ICFEToken>? _tokens;
+
+    private string? _res;
+
+    public CoreFilterReader(string resource)
+    {
+        _res = resource;
+        _tokens = new List<ICFEToken>();
+    }
+
+    public void GenerateTokenGroup()
+    {
+        for (int i = 0; i < _res?.Length; i++)
+        {
+            var ch = _res[i];
+            if (Char.IsDigit(ch))
+            {
+                // 生成VersionId的Token
+                string content = new string(ReadOnlySpan<char>.Empty);
+                
+                while (Char.IsDigit(ch) || ch == '.')
+                {
+                    content += ch;
+                    i++;
+                    ch = _res[i];
+                }
+
+                _tokens?.Add(new ICFEToken(content, TokenType.VersionId));
+            }
+            else if (Char.IsLetter(ch))
+            {
+                // 生成标识符的Token
+                string content = new string(ReadOnlySpan<char>.Empty);
+
+                while (Char.IsLetter(ch))
+                {
+                    content += ch;
+                    i++;
+                    ch = _res[i];
+                }
+                
+                _tokens?.Add(new ICFEToken(content, TokenType.Identifier));
+            }
+            else if (ch == '+')
+            {
+                _tokens?.Add(new ICFEToken("+", TokenType.ConnectOp));
+            }
+            else if (ch == ' ') continue;
+            else
+                throw new UnknownIcfeTokenException();
+        }
+    }
+
+    public List<ICFEToken>? GetTokens() => _tokens;
+}

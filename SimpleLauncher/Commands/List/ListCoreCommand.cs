d6fe7f03bad8de925264c6d87622b0acab687c4a
FZSGBall <692732163@qq.com>
2023-07-28 16:41:52 +0800

    Update

--- SimpleLauncher/Commands/List/ListCoreCommand.cs
+++ SimpleLauncher/Commands/List/ListCoreCommand.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MinecraftLaunch.Modules.Utils;
 
 namespace SimpleLauncher.Commands.List;
 internal sealed class ListCoreCommand : ISLCommand
@@ -24,60 +25,157 @@ internal sealed class ListCoreCommand : ISLCommand
 
     public async Task<ISLCommand?> ExecuteAsync(IEnumerable<string> args)
     {
-        SLOutput.Print("正在获取游戏核心列表，这可能会耗一些时间，请您耐心等待...\n", ConsoleColor.Yellow);
-
-        var res = (await GameCoreInstaller.GetGameCoresAsync()).Cores;
+        GameCoreUtil util = new GameCoreUtil();
+        var res = util.GetGameCores();
         
         if (!args.Any())
         {
-            Console.WriteLine("游戏核心搜索已完成，已获取核心列表: ");
-
+            if (res.ToArray().Length == 0)
+            {
+                SLCore.Utils.SLOutput.Print("搜索结果为空，您貌似还没有安装任何一个版本", ConsoleColor.Green);
+                return null;
+            }
+            
             foreach (var c in res)
-                Console.WriteLine($"{c.Type}: {c.Time} {c.Id}");
+                Console.WriteLine($"{c.Type}: {c.Id}");
         }
         else
         {
-            if (args.ToArray().Length > 1)
+            if (res.ToArray().Length == 0)
+            {
+                SLCore.Utils.SLOutput.Print("搜索结果为空，您貌似还没有安装任何一个版本", ConsoleColor.Green);
+                return null;
+            }
+            
+            if (args.ToArray().Length > 2)
                 throw CommandArgumentError.TooManyParameters;
             
             if (args.ToArray()[0] == "--release")
             {
-                Console.WriteLine($"游戏核心搜索已完成，已获取核心列表(您指定了筛选器: {args.ToArray()[0]}): ");
-                
                 res.ToList().ForEach(x =>
                 {
                     if (x.Type == "release")
                     {
-                        Console.WriteLine($"{x.Type}: {x.Time} {x.Id}");
+                        Console.WriteLine($"{x.Type}: {x.Id}");
                     }
                 });
             }
             else if (args.ToArray()[0] == "--snapshot")
             {
-                Console.WriteLine($"游戏核心搜索已完成，已获取核心列表(您指定了筛选器: {args.ToArray()[0]}): ");
-                
                 res.ToList().ForEach(x =>
                 {
                     if (x.Type == "snapshot")
                     {
-                        Console.WriteLine($"{x.Type}: {x.Time} {x.Id}");
+                        Console.WriteLine($"{x.Type}: {x.Id}");
                     }
                 });
             }
             else if (args.ToArray()[0] == "--old-alpha")
             {
-                Console.WriteLine($"游戏核心搜索已完成，已获取核心列表(您指定了筛选器: {args.ToArray()[0]}): ");
-
                 res.ToList().ForEach(x =>
                 {
                     if (x.Type == "old_alpha")
                     {
-                        Console.WriteLine($"{x.Type}: {x.Time} {x.Id}");
+                        Console.WriteLine($"{x.Type}: {x.Id}");
                     }
                 });
             }
             else
-                throw CommandArgumentError.WrongParameter;
+            {
+                if (args.ToArray()[0] == "--ava")
+                {
+                    var res_ava = (await GameCoreInstaller.GetGameCoresAsync()).Cores;
+
+                    if (args.ToArray().Length == 1)
+                    {
+                        foreach (var c in res_ava)
+                            Console.WriteLine($"{c.Type}: {c.Time} - {c.Id}");
+                        return null;
+                    }
+
+                    if (args.ToArray()[1] == "--release")
+                    {
+                        res_ava.ToList().ForEach(x =>
+                        {
+                            if (x.Type == "release")
+                            {
+                                Console.WriteLine($"{x.Type}: {x.Time} - {x.Id}");
+                            }
+                        });
+                    }
+                    else if (args.ToArray()[1] == "--snapshot")
+                    {
+                        res_ava.ToList().ForEach(x =>
+                        {
+                            if (x.Type == "snapshot")
+                            {
+                                Console.WriteLine($"{x.Type}: {x.Time} - {x.Id}");
+                            }
+                        });
+                    }
+                    else if (args.ToArray()[1] == "--old-alpha")
+                    {
+                        res_ava.ToList().ForEach(x =>
+                        {
+                            if (x.Type == "old_alpha")
+                            {
+                                Console.WriteLine($"{x.Type}: {x.Time} - {x.Id}");
+                            }
+                        });
+                    }
+                    else
+                        throw CommandArgumentError.WrongParameter;
+                }
+                else if (args.ToArray()[0] == "--local")
+                {
+                    if (res.ToArray().Length == 0)
+                    {
+                        SLCore.Utils.SLOutput.Print("搜索结果为空，您貌似还没有安装任何一个版本", ConsoleColor.Green);
+                        return null;
+                    }
+                    
+                    if (args.ToArray().Length == 1)
+                    {
+                        foreach (var c in res)
+                            Console.WriteLine($"{c.Type}: {c.Id}");
+                        return null;
+                    }
+                    
+                    
+                    if (args.ToArray()[1] == "--release")
+                    {
+                        res.ToList().ForEach(x =>
+                        {
+                            if (x.Type == "release")
+                            {
+                                Console.WriteLine($"{x.Type}: {x.Id}");
+                            }
+                        });
+                    }
+                    else if (args.ToArray()[1] == "--snapshot")
+                    {
+                        res.ToList().ForEach(x =>
+                        {
+                            if (x.Type == "snapshot")
+                            {
+                                Console.WriteLine($"{x.Type}: {x.Id}");
+                            }
+                        });
+                    }
+                    else if (args.ToArray()[1] == "--old-alpha")
+                    {
+                        res.ToList().ForEach(x =>
+                        {
+                            if (x.Type == "old_alpha")
+                            {
+                                Console.WriteLine($"{x.Type}: {x.Id}");
+                            }
+                        });
+                    }
+                    else
+                        throw CommandArgumentError.WrongParameter;
+                }
+            }
         }
 
         return null;

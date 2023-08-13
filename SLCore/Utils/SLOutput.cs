03b35f4262ce9b297634cfc1e88956172505b19c
FZSGBall <692732163@qq.com>
2023-08-13 20:32:15 +0800

    Update: Utils的整合

---
+++ SLCore/Utils/SLOutput.cs
@@ -0,0 +1,17 @@
+﻿namespace SLCore.Utils;
+
+public static class SLOutput
+{
+    public static void Print(string? text, ConsoleColor? color = null)
+    {
+        if (!color.HasValue)
+        {
+            Console.WriteLine(text);
+            return;
+        }
+        var previousColor = Console.ForegroundColor;
+        Console.ForegroundColor = color.Value;
+        Console.WriteLine(text);
+        Console.ForegroundColor = previousColor;
+    }
+}
